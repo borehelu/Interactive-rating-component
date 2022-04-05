b9cedcabde77639ac724694d109a0509e2e2660c
borehelu <borehelu@gmail.com>
2022-04-05 16:16:00 +0300

    modified rating

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 const btn = document.getElementById("rating_btn");
-const ratings = document.getElementsByName("li");
+const ratings = document.getElementsByTagName("li");
 const rating_card = document.getElementById("rating-card");
 const feedback_card = document.getElementById("thank-card");
+const rating_str = document.querySelector("p.selection");
+
+let rating_number = 0;
 
 
 btn.onclick = function(){
-    rating_card.style.display = "none";
-    feedback_card.style.display = "block";
+    if(rating_number != 0){
+        rating_card.style.display = "none";
+        feedback_card.style.display = "block";
+        rating_str.innerHTML = `You selected ${rating_number} out of 5`
+    }
     
 }
+
+
+
+for(let i = 0; i<ratings.length; i++){
+    ratings[i].onclick = function(){
+        let selected = document.querySelector("li.selected");
+        if(selected!=null){
+            selected.classList.remove("selected");
+        }
+        
+        ratings[i].classList.add("selected");
+        rating_number = i+1;
+    };
+    
+}
+
